a282a29b6a1c182ca30959ea06054668b820f652
Inar <Inar2000s@mail.ru>
2022-09-07 14:40:37 +0300

    fix: ESLint: TypeError: this.libOptions.parse is not a function  change eslint version to 8.22.0,

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [num, setNum] = useState(0)
-    const unused = 42
+  const [num, setNum] = useState(0);
+  const unused = 42;
 
-    const setNumber = () => {
-        const cat = {name: "Senya", age: 12}
-        console.log(cat)
-        setNum(num + 1)
-    }
+  const setNumber = () => {
+    const cat = { name: 'Senya', age: 12 };
+    console.log(cat);
+    setNum(num + 1);
+  };
 
-    return (
-        <div className="App">
-            <h1>Hello! React</h1>
-            <p>asdfasfdasfdasdf</p>
-            <p>Count:{num}</p>
-            <button onClick={setNumber}>Plus</button>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <h1>Hello! React</h1>
+      <p>asdfasfdasfdasdf</p>
+      <p>Count:{num}</p>
+      <button onClick={setNumber}>Plus</button>
+    </div>
+  );
 }
 
 export default App;
